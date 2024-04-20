9738d6e22b3649bd4c7f49655341a035c2c877d8
LHSang <lhsang64.contact@gmail.com>
2024-04-20 16:33:23 +0700

    Sang - init hardhat contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,15 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: "0.8.12",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  typechain: {
+    outDir: "./app/typechain",
+  },
 };
 
 export default config;
