428ce5c87842b47f384651f73d9ff642f77199ff
LHSang <lhsang64.contact@gmail.com>
2024-10-03 23:52:45 +0700

    reset data

--- src/utils/functions/formatCurrency.ts
+++
@@ -1,6 +0,0 @@
-export default function formatCurrencyWithCommas(number: any): string {
-  const numberWithCommas = number
-    .toString()
-    .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-  return numberWithCommas;
-}
--- src/utils/functions/formatDate.ts
+++
@@ -1,9 +0,0 @@
-import moment from "moment";
-
-const formatDate = (date: Date): string => {
-  moment.locale("en");
-  const formattedDate = moment(date).format("D/M/YYYY");
-  return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
-};
-
-export default formatDate;
