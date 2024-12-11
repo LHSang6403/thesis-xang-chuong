480513c5e146c824f9ab8a89e49a1e45bd762ace
LHSang <lhsang64.contact@gmail.com>
2024-12-12 00:11:12 +0700

    update model

--- data.py
+++ data.py
@@ -157,7 +157,7 @@ def splitData():
 
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-  return X_train, X_test, y_train, y_test
+  return X_train, X_test, y_train, y_test, scaler, data
 
 def main():
 
