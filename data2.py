cbe7301d7095cdec5398e4b05fa0feb1667d7c35
DinhChuong2909 <vudinhchuong2909@gmail.com>
2024-12-28 01:24:33 +0700

    Update

--- data2.py
+++ data2.py
@@ -4,6 +4,17 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 import datetime
+from torch.utils.data import TensorDataset, DataLoader
+import torch
+
+# Create a function to create sequences of 
+def create_sequences(data, seq_length):
+  X, y = [], []
+  for i in range(len(data) - seq_length):
+      X.append(data[i:i+seq_length])
+      y.append(data[i+seq_length])
+  return np.array(X), np.array(y)
+
 
 def splitData():
   data = pd.read_csv('./newfile.csv')
@@ -12,24 +23,34 @@ def splitData():
   data['Date'] = pd.to_datetime(data['Date'])
   data.set_index('Date', inplace=True)
 
-  print("Columns in the data:", data.columns)
-
   scaler = MinMaxScaler()
   scaled_data = scaler.fit_transform(data)
-
-  print("Columns in the data:", data.columns)
-
-  print(data.describe())
-  print("Number of NaN values in each column:")
-  print(data.isnull().sum())
-  print("Check for inf/-inf values:")
-  print(data[data.isin([np.inf, -np.inf]).any(axis=1)])
 
   # Remove rows with NaN or inf values
   data = data.dropna()  # Drop rows containing NaN
   data = data.replace([np.inf, -np.inf], np.nan).dropna()  # Remove inf/-inf
 
   scaled_data = pd.DataFrame(scaled_data, columns=data.columns, index=data.index)
+
+  # train = data.loc['2024-11-18 14:30:00+00:00':'2024-12-12 19:40:00+00:00']
+  # test = data.loc['2024-12-12 19:41:00+00:00':'2024-12-18 16:52:00+00:00']
+
+  # train_scaled = scaler.fit_transform(train)
+  # test_scaled = scaler.fit_transform(test)
+
+  # # Create sequences with a length of 100
+  # seq_length = 100
+  # X_train, y_train = create_sequences(train_scaled, seq_length)
+  # X_test, y_test = create_sequences(test_scaled, seq_length)
+
+  # # Create data loaders
+  # train_data = TensorDataset(torch.tensor(X_train).float(), torch.tensor(y_train).float())
+  # test_data = TensorDataset(torch.tensor(X_test).float(), torch.from_numpy(y_test).float())
+
+  # batch_size = 64
+  # train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
+  # test_loader = DataLoader(test_data, shuffle=False, batch_size=batch_size)
+
 
   # Time steps
   timesteps = 100
@@ -46,16 +67,19 @@ def splitData():
   X = np.array(X)
   y = np.array(y)
 
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
 
-  X_train_df = pd.DataFrame(X_train.reshape(X_train.shape[0], -1))
-  print("20 dòng cuối của tập X_train:")
-  print(X_train_df.tail(20).to_string(index=False))
+  print(f"X_train shape: {X_train.shape}")
+  print(f"y_train shape: {y_train.shape}")
+  print(f"X_test shape: {X_test.shape}")
+  print(f"y_test shape: {y_test.shape}")
+  
+  # for each in y_test:
+  #   print(each)
 
-  X_test_df = pd.DataFrame(X_test.reshape(X_test.shape[0], -1))
-  print("20 dòng cuối của tập X_test:")
-  print(X_test_df.tail(20).to_string(index=False))
+  # print(f"train_loader: {len(train_loader)}")
+  # print(f"test_loader: {len(test_loader)}")
 
   return X_train, X_test, y_train, y_test, scaler, data
 
-print(splitData())
+splitData()
